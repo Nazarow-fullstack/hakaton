4fa6b13e634f92450e357159ed209db2ad496b2e
Mangyec2009 <amin.tj2010@gmail.com>
2025-02-19 16:34:07 +0500

    Creating page About

---
+++ src/app/about/page.js
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import React from 'react'
+
+const About = () => {
+  return <>
+    <div>
+        <div className='flex items-center justify-between'>
+            <img className="" src="https://www.thelifeyoucansave.org/wp-content/uploads/2019/11/Screen-Shot-2015-07-13-at-1.53.34-PM.png" alt="" />
+            <p></p>
+        </div>
+    </div>
+  </>
+}
+
+export default About
