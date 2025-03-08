d34636b5a0b26f83326b93989fa1e4f6e1f5a389
Mangyec2009 <amin.tj2010@gmail.com>
2025-03-08 14:11:36 +0500

    update idk

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Projects = () => {
   return <>
-    <div className='text-[white] font-'>
-        Projects
+    <div className='max-w-[1200px] mx-auto my-[20px] text-[white] '>
+        <h1 className='text-[25px] font-semibold text-center mb-[15px]'>Why There Are Projects?</h1>
+        <p className='text-[14px] mx-auto text-center w-[60%]'>We believe in making a difference by supporting those in need. Our projects focus on providing essential resources, funding, and support to communities, organizations, and individuals facing hardships.</p>
     </div>
   </>
 }
